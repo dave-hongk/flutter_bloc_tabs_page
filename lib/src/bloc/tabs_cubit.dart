bd82f9cc8d89080c060db00c0d3a64a07cf692d4
dave-hongk <dave.hongk@gmail.com>
2021-12-19 19:28:12 -0800

    Added TabsCubit to handle selected tabs and TabState emits.

---
+++ lib/src/bloc/tabs_cubit.dart
@@ -0,0 +1,16 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc_tabs_page/src/bloc/tabs_state.dart';
+
+/// {@template tabs_cubit}
+/// [Cubit] to handle [Tab] selections. Emits new [TabState] on changes.
+/// {@endtemplate tabs_cubit}
+class TabsCubit extends Cubit<TabState> {
+  /// {@macro tabs_cubit}
+  TabsCubit() : super(TabState());
+
+  /// [Tab] selected.
+  void tabSelected(Tab tab) {
+    if (tab == state.selected) return;
+    emit(TabState((b) => b..selected = tab));
+  }
+}
---
+++ test/bloc/tabs_cubit_test.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_bloc_tabs_page/src/bloc/tabs_cubit.dart';
+import 'package:flutter_bloc_tabs_page/src/bloc/tabs_state.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('TabsCubit', () {
+    late TabsCubit cubit;
+    setUp(() {
+      cubit = TabsCubit();
+    });
+    group('initialization', () {
+      test('Initial tab state', () {
+        expect(cubit.state, TabState());
+      });
+    });
+
+    group('tabSelected', () {
+      blocTest<TabsCubit, TabState>(
+        'transition to new tab emits new state',
+        build: () => TabsCubit(),
+        act: (cubit) => cubit.tabSelected(Tab.tab2),
+        expect: () => <TabState>[TabState((b) => b..selected = Tab.tab2)],
+      );
+
+      blocTest<TabsCubit, TabState>(
+        'same as already selected does not emit new state',
+        build: () => TabsCubit(),
+        act: (cubit) => cubit.tabSelected(Tab.tab1),
+        expect: () => [],
+      );
+    });
+  });
+}
